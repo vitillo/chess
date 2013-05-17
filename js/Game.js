7c0ecc11a27d905e31a2f0028874cfed164b3d83
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2013-05-17 15:49:37 -0700

    Cleanup.

--- js/Game.js
+++ js/Game.js
@@ -7,11 +7,12 @@ function Game(container){
 Game.prototype = {
   _initialize: function(container){
     var domContainer = document.getElementById(container);
-    domContainer.innerHTML = "<canvas></canvas>";
+    domContainer.style.position = "relative";
+    domContainer.innerHTML = "<canvas style='display:block;'></canvas>";
     var canvas = domContainer.firstChild;
  
     this.renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true}); 
-    this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+    this.renderer.setSize( window.innerWidth, window.innerHeight); 
     this.renderer.setClearColor(0x8CBED6);
 
     this.hud = new Hud(this.renderer.domElement);
@@ -19,7 +20,7 @@ Game.prototype = {
     this.scene = new THREE.Scene(); 
     this.board = new Board(this);
 
-    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.2, 1000 ); 
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.2, 1000); 
     this.camera.position.z = 10;
     this.camera.position.y = 10;
     this.camera.lookAt(new THREE.Vector3(0, 0, 2));
--- js/Hud.js
+++ js/Hud.js
@@ -3,7 +3,7 @@ function Hud(canvas){
   this.text.id = "hud";
   this.text.style.width = "420px";
   this.text.style.height = "100px";
-  this.text.style.top = "20px";
+  this.text.style.top = "15px";
   this.text.style.left = "20px";
   this.text.style.position = "absolute";
   this.text.style.font = "16px 'Lucida Grande', tahoma, verdana, arial, sans-serif";
